374cf12aef7cfcfcd240ea2d75ffab5dd7e27951
jakeengstrom3 <jakeengstrom3@gmail.com>
2022-05-15 19:57:40 -0700

    add setup call to province_test to create fresh province fixture for each test method

--- test/province_test.rb
+++ test/province_test.rb
@@ -6,14 +6,18 @@ require_relative '../lib/data'
 
 class ProvinceTest < Minitest::Test
 
+  def setup
+     @asia = Province.new(sample_province_data)
+  end
+
   def test_province_shortfall
-    asia = Province.new(sample_province_data)
-    assert_equal(5, asia.shortfall)
+    @asia = Province.new(sample_province_data)
+    assert_equal(5, @asia.shortfall)
   end
   
   def test_province_profit
-    asia = Province.new(sample_province_data)
-    assert_equal(230, asia.profit)
+    @asia = Province.new(sample_province_data)
+    assert_equal(230, @asia.profit)
   end
 
 end
